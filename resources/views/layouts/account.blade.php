306e74a0eaef0b5a74a76d12c4d268f3635094d9
anharsaja <anharmuk12@gmail.com>
2023-11-25 22:35:45 +0700

    update

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -112,7 +112,7 @@
                     </ul>
 
                     <!-- <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-                        <a href="" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                        <a href="git@github.com:anharsaja/money-management.git" class="btn btn-primary btn-lg btn-block btn-icon-split">
                             <i class="fab fa-github"></i> GITHUB
                         </a>
                     </div> -->
